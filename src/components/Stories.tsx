aa9e602cd199e6283cf4a6fe7607bde33cc29017
thiagoifrn <thiagoifrn@gmail.com>
2024-08-23 02:12:13 -0300

    feat: add story component

---
+++ src/components/Stories.tsx
@@ -0,0 +1,133 @@
+import Image from "next/image";
+
+const Stories = async () => {
+  return (
+    <div className="p-4 bg-white shadow-md rounded-lg text-xs overflow-scroll scrollbar-h scrollbar-hide">
+      {/* STORY */}
+      <div className="flex gap-8 w-max">
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src={"/noAvatar.png"}
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">thiago</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src={"/noAvatar.png"}
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">thiago</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src={"/noAvatar.png"}
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">thiago</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src={"/noAvatar.png"}
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">thiago</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src={"/noAvatar.png"}
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">thiago</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src={"/noAvatar.png"}
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">thiago</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src={"/noAvatar.png"}
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">thiago</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src={"/noAvatar.png"}
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">thiago</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src={"/noAvatar.png"}
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">thiago</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src={"/noAvatar.png"}
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">thiago</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src={"/noAvatar.png"}
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">thiago</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src={"/noAvatar.png"}
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">thiago</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stories;
